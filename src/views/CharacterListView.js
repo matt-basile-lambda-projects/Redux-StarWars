074864e162ceb212517a7abbc6c6226a765567a7
Matt Basile <matthewbasile2@gmail.com>
2019-01-30 16:03:58 -0500

    Completed MBP... those reducers can trip you up when seperated

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,24 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import {getChars} from "../actions"
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
+    console.log('mountings')
     // call our action
+    this.props.getChars();
   }
 
   render() {
-    if (this.props.fetching) {
+    console.log(this.props.characters)
+    if (this.props.isLoading) {
+      <h1>Data is Loading...</h1>
       // return something here to indicate that you are fetching data
     }
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
+        <CharacterList characters={this.props.characters} isLoading={this.props.isLoading}/>
       </div>
     );
   }
@@ -27,9 +31,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  error: state.charsReducer.error,
+  isLoading: state.charsReducer.isLoading
+});
 export default connect(
-  null /* mapStateToProps replaces null here */,
+   mapStateToProps,
   {
-    /* action creators go here */
+   getChars
   }
 )(CharacterListView);
